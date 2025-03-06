bbb6f8eded108e818161eed1f2f440f3f311a1e5
fqb22153 <kieran.ballard.2022@uni.strath.ac.uk>
2025-03-06 12:50:36 +0000

    use state

--- mobile/app/(app)/log.tsx
+++ mobile/app/(app)/log.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import { View, Text } from "react-native";
 
 export default function Log() {
 
+
+  const [count, setCount] = useState(0);
+
   return (
     <View>
+
       <Text style={{ color: "#ff0000" }}>Log page</Text>
       <Text> Kierans Job 🤓 </Text>
+      <Text>Date: {count} </Text> 
+      <Text onPress={() => {setCount(count + 1)}}>INcrement</Text> 
     </View>
   );
 }
