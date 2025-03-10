cc1ae3b610dde52067d8e76550e843ac2ab8d2ee
Ethan Midgley <ethan.midgley.2022@uni.strath.ac.uk>
2025-03-10 13:51:25 +0000

    Easier fetching hooks

---
+++ mobile/hooks/useApi.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const API_PATH = "http://10.12.123.227:3000";
+
+type FetchStatus = "loading" | "error" | "success" | "not called";
+
+type ApiResponse<DataType> = {
+  data: DataType | null;
+  status: FetchStatus;
+  error: string | null;
+};
+
+export const useApi = <DataType,>(
+  url: string,
+  options?: RequestInit,
+): [() => Promise<ApiResponse<DataType>>, ApiResponse<DataType>] => {
+  const [data, setData] = useState<DataType | null>(null);
+  const [status, setStatus] = useState<FetchStatus>("not called");
+  const [error, setError] = useState<string | null>(null);
+
+  const send = async (): Promise<ApiResponse<DataType>> => {
+    setStatus("loading");
+    setData(null);
+    setError(null);
+
+    try {
+      const res = await fetch(API_PATH + url, options);
+      const json = (await res.json()) as DataType;
+
+      setData(json);
+      setStatus("success");
+    } catch (err: any) {
+      setStatus("error");
+      setError(err.message);
+    }
+
+    return { data, status, error };
+  };
+
+  return [send, { data, status, error }];
+};
+
+// a query is an api with different preset values and automatically called
+
+export default useApi;
