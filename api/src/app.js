52fa84fb48e21bafdf9c84e25e6bbf2d9194af02
Ethan Midgley <ethan.midgley.2022@uni.strath.ac.uk>
2025-03-13 17:41:03 +0000

    Done post fetching

--- api/src/app.js
+++ api/src/app.js
@@ -58,7 +58,41 @@ const main = async () => {
   //posts endpoints
   app.post("/posts/add", async (request, response) => {});
 
-  app.get("/posts/fetch", async (request, response) => {});
+  app.get("/posts/fetch", async (request, response) => {
+    const result = [];
+    try {
+      const [posts] = await db.query(
+        "SELECT cbp.id, cbp.title, cbp.image, cbp.date, cbp.description, cbu.full_name as full_name, cbc.time, cbc.`level`, cbc.lat, cbc.lon FROM `CS317-bldr-posts` cbp LEFT JOIN `CS317-bldr-users` cbu on cbp.user_id = cbu.id LEFT JOIN `CS317-bldr-climbs` cbc on cbp.climb_id = cbc.id",
+      );
+
+      for (const post of posts) {
+        const [comments] = await db.query(
+          "SELECT cbu.full_name as author, cbc.date, cbc.content FROM `CS317-bldr-comments` cbc LEFT JOIN `CS317-bldr-users` cbu ON cbu.id = cbc.user_id WHERE cbc.post_id = ?",
+          [post.id],
+        );
+
+        result.push({
+          id: post.id,
+          title: post.title,
+          image: post.image,
+          author: post.full_name,
+          climb: {
+            time: post.time,
+            level: post.level,
+            lat: post.lat,
+            lon: post.lon,
+          },
+          comments: comments,
+        });
+      }
+
+      response.json(result);
+    } catch (err) {
+      console.log(err);
+      response.status(500).send({ error: "Failed" });
+      return;
+    }
+  });
 
   //comments endpoints
   app.post("/comments/add", async (request, response) => {});
