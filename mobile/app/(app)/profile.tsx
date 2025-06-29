d2143a907eceb8992b65fa9e20812c69222452e6
Ethan Midgley <ethan.midgley.2022@uni.strath.ac.uk>
2025-06-29 20:11:58 +0100

    edit biooo

--- mobile/app/(app)/profile.tsx
+++ mobile/app/(app)/profile.tsx
@@ -2,10 +2,21 @@ import React, { useEffect, useState } from "react";
 import { styles } from "@/constants/style";
 import { useQuery } from "@/hooks/useQuery";
 import { useLocalSearchParams } from "expo-router";
-import { ActivityIndicator, FlatList, Text, TouchableOpacity, View } from "react-native";
+import {
+  ActivityIndicator,
+  Alert,
+  FlatList,
+  Modal,
+  Pressable,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Image } from "expo-image";
 import { API_PATH } from "@/hooks/useApi";
 import { Post, PostComponent } from "@/components/PostComponent";
+import { useMutation } from "@/hooks/useMutation";
 
 type User = {
   fullname: string;
@@ -18,77 +29,196 @@ type UserPostsResponse = {
   posts: Post[];
 };
 
-const changeBio = () => {
-  
-}
-
 export default function Profile() {
+  const [editVisible, setEditVisible] = useState<boolean>(false);
+
+  const [updateBio] = useMutation("/profile/bio-edit");
+
   const { user_id } = useLocalSearchParams<{ user_id: string }>();
-  const { data, status } = useQuery<User>("/user/" + user_id);
-  const { data: postData, status: postStatus, refetch } = useQuery<UserPostsResponse>("/posts/fetchbyuser/" + user_id);
+  const {
+    data,
+    status,
+    refetch: refetchUserData,
+  } = useQuery<User>("/user/" + user_id);
+  const [bio, setBio] = useState<string>("");
+  const {
+    data: postData,
+    status: postStatus,
+    refetch,
+  } = useQuery<UserPostsResponse>("/posts/fetchbyuser/" + user_id);
 
   return (
     <View key={user_id}>
       {status === "success" ? (
         <>
+          <Modal animationType="slide" transparent={true} visible={editVisible}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  width: 300,
+                  backgroundColor: "white",
+                  borderRadius: 20,
+                  padding: 20,
+                }}
+              >
+                <Text style={styles.headingLarge}>Edit Bio</Text>
+
+                <TextInput
+                  style={[styles.input, { width: "100%", marginTop: 20 }]}
+                  value={bio}
+                  placeholder="Enter bio"
+                  onChangeText={setBio}
+                />
+
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    columnGap: 20,
+                  }}
+                >
+                  <Pressable
+                    style={[styles.button, { flex: 1, height: 30 }]}
+                    onPress={() => setEditVisible(false)}
+                  >
+                    <Text style={styles.button_text}>Close</Text>
+                  </Pressable>
+                  <Pressable style={[styles.button, { flex: 1, height: 30 }]}>
+                    <Text
+                      style={styles.button_text}
+                      onPress={async () => {
+                        const { status } = await updateBio({
+                          body: { bio: bio },
+                        });
+                        setEditVisible(false);
+                        if (status === "success") {
+                          Alert.alert("Updated bio");
+                          refetchUserData();
+                        } else {
+                          Alert.alert("Failed to update bio, try again later");
+                        }
+                      }}
+                    >
+                      Save
+                    </Text>
+                  </Pressable>
+                </View>
+              </View>
+            </View>
+          </Modal>
+
           <FlatList
             ListHeaderComponent={() => (
               <>
-          {/* main profile div */}
-          <View style={{ width: "100%" }}>
-            <View style={{ flex: 1, width:"30%", flexDirection: "row", alignItems: "center", marginHorizontal: 8, marginTop: 14 }}>
-              <Image
-                source={`${API_PATH}/image/${data?.image}`}
-                contentFit="cover"
-                style={{ width: "100%", borderRadius: "50%", height: 120, marginLeft: 5, marginTop: 5, borderWidth: 5, borderColor: "#f00"}}
-              />
+                {/* main profile div */}
+                <View style={{ width: "100%" }}>
+                  <View
+                    style={{
+                      flex: 1,
+                      width: "30%",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      marginHorizontal: 8,
+                      marginTop: 14,
+                    }}
+                  >
+                    <Image
+                      source={`${API_PATH}/image/${data?.image}`}
+                      contentFit="cover"
+                      style={{
+                        width: "100%",
+                        borderRadius: "50%",
+                        height: 120,
+                        marginLeft: 5,
+                        marginTop: 5,
+                        borderWidth: 5,
+                        borderColor: "#f00",
+                      }}
+                    />
 
-              <View style={{ marginHorizontal: 20, height: 120, width: "190%" }}>
-                  <Text style={{ ...styles.headingLarge, textAlign: "center" }}>{data?.fullname}</Text>
-              </View>
-            </View>
+                    <View
+                      style={{
+                        marginHorizontal: 20,
+                        height: 120,
+                        width: "190%",
+                      }}
+                    >
+                      <Text
+                        style={{ ...styles.headingLarge, textAlign: "center" }}
+                      >
+                        {data?.fullname}
+                      </Text>
+                    </View>
+                  </View>
 
-            <View style={{ height: 80, marginHorizontal: 20, marginTop: 15, width: "90%" }}>
-              <Text 
-                style={{ ...styles.text, textAlign: "justify" }}>{data?.bio}
-              </Text>
-              <TouchableOpacity
-                onPress={() => changeBio()}>
-                <Text style={{ ...styles.button_text, color: "#f00", marginTop: 5, marginBottom: 15}}>
-                Edit Bio
-                </Text>
-              </TouchableOpacity>
-            </View>
+                  <View
+                    style={{
+                      height: 80,
+                      marginHorizontal: 20,
+                      marginTop: 15,
+                      width: "90%",
+                    }}
+                  >
+                    <Text style={{ ...styles.text, textAlign: "justify" }}>
+                      {data?.bio}
+                    </Text>
+                    <TouchableOpacity onPress={() => setEditVisible(true)}>
+                      <Text
+                        style={{
+                          ...styles.button_text,
+                          color: "#f00",
+                          marginTop: 5,
+                          marginBottom: 15,
+                        }}
+                      >
+                        Edit Bio
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
 
-            {/* uploads div */}
-            </View>
-            </>
+                  {/* uploads div */}
+                </View>
+              </>
             )}
-                ListEmptyComponent={() => (
-                  <Text style={{ ...styles.headingMedium, paddingTop: 150, textAlign: "center" }}>This user has no posts</Text>
-                )}
-                style={{}}
-                refreshing={postStatus=== "loading"}
-                onRefresh={refetch}
-                data={postData?.posts}
-                onEndReachedThreshold={2}
-                onEndReached={async () => {
-                  if (postData?.next_cursor != null) {
-                    await refetch(
-                      {
-                        params: { next_cursor: postData?.next_cursor },
-                      },
-                      {
-                        refetchPolicy: (oldData, newData) => ({
-                          posts: [...oldData.posts, ...newData.posts],
-                          next_cursor: newData.next_cursor,
-                        }),
-                      },
-                    );
-                  }
+            ListEmptyComponent={() => (
+              <Text
+                style={{
+                  ...styles.headingMedium,
+                  paddingTop: 150,
+                  textAlign: "center",
                 }}
-                renderItem={(d) => <PostComponent {...d.item} />}
-              />
+              >
+                This user has no posts
+              </Text>
+            )}
+            style={{}}
+            refreshing={postStatus === "loading"}
+            onRefresh={refetch}
+            data={postData?.posts}
+            onEndReachedThreshold={2}
+            onEndReached={async () => {
+              if (postData?.next_cursor != null) {
+                await refetch(
+                  {
+                    params: { next_cursor: postData?.next_cursor },
+                  },
+                  {
+                    refetchPolicy: (oldData, newData) => ({
+                      posts: [...oldData.posts, ...newData.posts],
+                      next_cursor: newData.next_cursor,
+                    }),
+                  },
+                );
+              }
+            }}
+            renderItem={(d) => <PostComponent {...d.item} />}
+          />
         </>
       ) : (
         <ActivityIndicator size="large" />
--- mobile/hooks/useApi.tsx
+++ mobile/hooks/useApi.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export const API_PATH =
   process.env.EXPO_PUBLIC_API_URL ||
-  "https://devweb2024.cis.strath.ac.uk/mhb22136-nodejs";
+  "https://devweb2024.cis.strath.ac.uk/ksb22167-nodejs";
 
 type TransmissionData = {
   body?: object;
